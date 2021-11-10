dab1f78bb39155e7d75388c58459f791532e7da9
patriklussi <patrik.lussi@gmail.com>
2021-11-10 11:30:41 +0100

    Crazy hamburger

--- examination2-bonz.ai/src/views/Home.vue
+++ examination2-bonz.ai/src/views/Home.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navigation"><h1>Bonz.Ai</h1></nav>
+    <header class="pageHeader"></header>
+    <main class="mainSection"></main>
+    <footer class="pageFooter"></footer>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.navigation {
+  width: 100%;
+  height: 132px;
+  background: #00C2BA;
+}
 </style>
