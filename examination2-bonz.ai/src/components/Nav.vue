2586c51aa5884ef8d7e525089c29933d89e65096
patriklussi <46919194+patriklussi@users.noreply.github.com>
2021-11-11 12:18:43 +0100

    A lot of shit is done on homepage

--- examination2-bonz.ai/src/components/Nav.vue
+++ examination2-bonz.ai/src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper">
+  <div class="nav-wrapper">
       <nav class="navigation">
     <h1>Bonz.Ai</h1> 
     <div class="navWrap">
--- examination2-bonz.ai/src/views/Home.vue
+++ examination2-bonz.ai/src/views/Home.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="home">
-    <Nav/>
-    <header class="pageHeader"></header>
-    <main class="mainSection"></main>
+  <div class="wrapper">
+    <Nav />
+    <header class="pageHeader">
+      <article class="titleBox">
+        <h1>Bonz.Ai</h1>
+        <h2>An Ai-Powered Eco-Stay</h2>
+      </article>
+    </header>
+    <main class="mainSection">
+      <h2>Bonz.Ai</h2>
+      <p>
+        Hello from Bonz.AI 👋 we are thrilled to welcome you to our AI-powered
+        eco-stay at Bohuslän to the north of Gothenburg, Sweden.
+        
+        <br>
+        <br>
+      
+         We believe in
+        offering a personalized stay like none other, by bringing the ultimate
+        contact-less experience to you at your finger tips - on your mobile
+        device. Sit back, relax and let Bonz.AI take care of you.
+      </p>
+      <button class="generalButton">Book now</button>
+    </main>
+    <section class="roomView"></section>
     <footer class="pageFooter"></footer>
-   
   </div>
 </template>
 
@@ -13,15 +33,69 @@
 import Nav from "../components/Nav.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-  Nav
-  }
-}
+    Nav,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+@import "../scss/_mixins.scss";
+@import "../scss/_components.scss";
+.wrapper {
+  @include grid-column-auto;
+}
+.pageHeader {
+  height: 810px;
+  //  @include center;
+  // flex-direction: column;
 
-
-
+  .titleBox {
+    border-bottom: 6px solid #08f7fe;
+    width: 642px;
+    display: flex;
+    flex-direction: column;
+    margin-left: 167px;
+    margin-top: 91px;
+    h1 {
+      font-size: 4rem;
+      color: #00c2ba;
+      align-self: center;
+      margin-bottom: 50px;
+    }
+    h2 {
+      font-size: 3rem;
+      font-weight: lighter;
+      color: white;
+      align-self: center;
+      margin-bottom: 39px;
+    }
+  }
+  background: url("../assets/photos/02-night.jpg");
+  background-repeat: no-repeat, repeat;
+  background-size: cover;
+  background-position: center;
+}
+.mainSection {
+  height: 646px;
+  background: #d3cfc347;
+ @include center;
+ flex-direction: column;
+ p {
+   width:60%;
+   font-size: 2rem;
+   margin:0;
+ }
+ h2 {
+   font-size: 2.25rem;
+  
+ }
+ .generalButton {
+ 
+ }
+}
+.roomView {
+  height: 859px;
+}
 </style>
