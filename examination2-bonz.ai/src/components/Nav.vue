7dc9c72924626e3d626f93042bd71a22577f8441
patriklussi <46919194+patriklussi@users.noreply.github.com>
2021-11-11 11:10:46 +0100

    Jaja

---
+++ examination2-bonz.ai/src/components/Nav.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+      <nav class="navigation">
+    <h1>Bonz.Ai</h1> 
+    <div class="navWrap">
+    <h2>Stay</h2>
+    <h2>About</h2>
+    <button class="generalButton">Book</button>
+    </div>
+       <article class="rectangle"></article>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style  lang="scss" scoped>
+@import '../scss/_mixins.scss';
+@import '../scss/_components.scss';
+.navigation {
+  width: 100%;
+ 
+  height: 132px;
+  background: #00C2BA;
+  @include center;
+  justify-content: space-between;
+  h2 {
+    font-weight: 700;
+  }
+  h2:hover {
+    cursor: pointer;
+  }
+ 
+  .navWrap {
+    @include center;
+    width: 402px;
+    justify-content: space-between;
+    margin-right: 92px;
+  }
+ h1 {
+   margin-left: 89px;
+ }
+  .rectangle {
+  width:100%;
+  height: 11px;
+  background: #08F7FE;
+  position: absolute;
+  top:121px;
+
+
+} 
+}
+</style>
--- examination2-bonz.ai/src/views/Home.vue
+++ examination2-bonz.ai/src/views/Home.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="home">
-    <nav class="navigation">
-    <h1>Bonz.Ai</h1> 
-    <div class="navWrap">
-    <h2>Stay</h2>
-    <h2>About</h2>
-    <button class="generalButton">Book</button>
-    </div>
-       <article class="rectangle"></article>
-    </nav>
+    <Nav/>
     <header class="pageHeader"></header>
     <main class="mainSection"></main>
     <footer class="pageFooter"></footer>
@@ -18,51 +10,18 @@
 
 <script>
 // @ is an alias to /src
-
+import Nav from "../components/Nav.vue";
 
 export default {
   name: 'Home',
   components: {
-  
+  Nav
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '../scss/_mixins.scss';
-@import '../scss/_components.scss';
-.navigation {
-  width: 100%;
- 
-  height: 132px;
-  background: #00C2BA;
-  @include center;
-  justify-content: space-between;
-  h2 {
-    font-weight: 700;
-  }
-  h2:hover {
-    cursor: pointer;
-  }
- 
-  .navWrap {
-    @include center;
-    width: 402px;
-    justify-content: space-between;
-    margin-right: 92px;
-  }
- h1 {
-   margin-left: 89px;
- }
-  .rectangle {
-  width:100%;
-  height: 11px;
-  background: #08F7FE;
-  position: absolute;
-  top:121px;
 
 
-} 
-}
 
 </style>
